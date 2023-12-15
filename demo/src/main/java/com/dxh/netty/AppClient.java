6308dcc159212f16f6a88d8d175956f42a21e623
dxhInter <dxh0530@gmail.com>
2023-12-14 22:33:43 -0500

    update a Netty demo

--- demo/src/main/java/com/dxh/netty/AppClient.java
+++ demo/src/main/java/com/dxh/netty/AppClient.java
@@ -6,8 +6,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.sctp.nio.NioSctpChannel;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.CharsetUtil;
 
